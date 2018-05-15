8f68d49c83043c1687b0273de22921eb90c88916
piotrbaranski <p.baranski@yahoo.pl>
2018-05-15 06:16:28 +0200

    Add more solutions.

---
+++ 7kyu/Find the capitals.js
@@ -0,0 +1,15 @@
+/**Instructions
+ Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+ Example
+ Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );**/
+
+var capitals = function (word) {
+    var capitalTable = [];
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] === word[i].toUpperCase()) {
+            capitalTable.push(i);
+        }
+    }
+    return capitalTable;
+};
